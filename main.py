fb425303fdd59239bd3c8f6c2c5ab05b2273ce0e
Alex <49818986+aleexv6@users.noreply.github.com>
2024-02-28 19:02:20 +0100

    starting physique and futures data in webapp

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 import database as db
+import pandas as pd
 
 app = Flask(__name__)
 
-physique = db.get_database_physical()
-euronext = db.get_database_euronext()
+expi = {'Ble tendre' : 'MAR24', 'Mais' : 'MAR24', 'Colza' : 'MAY24'}
+listProductFutures = {'Ble tendre':'EBM', 'Mais':'EMA', 'Colza':'ECO'}
 
+cursorPhysique = db.get_database_physical().find({})
+dfPhysique = pd.DataFrame(list(cursorPhysique)).sort_values(by='Date', ascending=True)
+productPhysique = dfPhysique['Produit'].unique()
+
+cursorFutures = db.get_database_euronext().find({})
+dfFutures = pd.DataFrame(list(cursorFutures)).sort_values(by='Date', ascending=True)
+productFutures = dfFutures['Ticker'].unique()
 
 @app.route("/")
-def hello_world():
-    return f"<p>Hello, World !</p>"
+def index():
+    return render_template('index.html')
+
+@app.route("/physique")
+def physique():
+    return render_template('physique.html', data=dfPhysique, listProduct=productPhysique, datafutures=dfFutures, listProductFutures=listProductFutures, expi=expi)
+
+@app.route("/futures")
+def futures():
+    return render_template('futures.html', listProduct=productFutures, data=dfFutures, listProductFutures=listProductFutures)
     
