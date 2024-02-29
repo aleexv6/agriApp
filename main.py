b08d0fc3c829c07cf3bb038d30008a2b12a47954
Alex <49818986+aleexv6@users.noreply.github.com>
2024-02-29 13:26:15 +0100

    start highchart for charting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import database as db
 import pandas as pd
+from highcharts_core import highcharts
 
 app = Flask(__name__)
 
@@ -9,6 +10,10 @@ listProductFutures = {'Ble tendre':'EBM', 'Mais':'EMA', 'Colza':'ECO'}
 
 cursorPhysique = db.get_database_physical().find({})
 dfPhysique = pd.DataFrame(list(cursorPhysique)).sort_values(by='Date', ascending=True)
+dfPhysiqueEBM = dfPhysique[(dfPhysique['Produit'] == 'Ble tendre') & (dfPhysique['Place'] == 'La Pallice Rendu')]
+dfPhysiqueEBM = dfPhysiqueEBM[['Date', 'Prix']]
+dfPhysiqueEBM = dfPhysiqueEBM[dfPhysiqueEBM['Date'].dt.year > 2023]
+
 productPhysique = dfPhysique['Produit'].unique()
 
 cursorFutures = db.get_database_euronext().find({})
@@ -21,6 +26,11 @@ def index():
 
 @app.route("/physique")
 def physique():
+    my_chart = highcharts.Chart.from_pandas(dfPhysiqueEBM,
+                                            property_map = {'x': 'Date', 'y': 'Prix'},
+                                            chart_kwargs = {'container':'myChartBase', 'variable_name': 'myChart'},
+                                            options_kwargs={'title': {'text': 'Abrakadabra'}, 'x_axis': {'type': 'datetime'}})
+    my_chart.to_js_literal('static/js/my-js-literal.js')
     return render_template('physique.html', data=dfPhysique, listProduct=productPhysique, datafutures=dfFutures, listProductFutures=listProductFutures, expi=expi)
 
 @app.route("/futures")
