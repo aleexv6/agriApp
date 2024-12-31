145daa014b585b00b2952bcc1136a26ef8321d29
Alexandre L <49818986+aleexv6@users.noreply.github.com>
2024-12-31 14:17:52 +0100

    localStorage for dark-theme

---
+++ static/js/darkmode.js
@@ -0,0 +1,26 @@
+let darkmode = localStorage.getItem('darkmode')
+const elements = document.getElementsByClassName('highcharts-chart-theme-responsive');
+const themeSwitch = document.getElementById('themeToggle')
+
+const enableDarkmode = () => {
+    document.body.classList.add('dark-theme')
+    Array.from(elements).forEach(element => {
+        element.classList.add('highcharts-dark')
+    });
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('dark-theme')
+    Array.from(elements).forEach(element => {
+        element.classList.add('highcharts-light')
+    });
+    localStorage.removeItem('darkmode');
+}
+
+if (darkmode === 'active') enableDarkmode()
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
